c5cd7c301687793bf2d16e9d5c3eabe992b3507e
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-03 22:08:13 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import WelcomeMessage from './WelcomeMessage
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
