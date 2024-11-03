f7616dacf2f8d5e3b57cb0064889870c61479dae
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-03 22:12:08 +0100

    Update Footer.jsx

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,3 +1,12 @@
-<footer>
-  <p>© 2023 City Lovers</p>
-</footer>
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
