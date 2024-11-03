4286f54ded08bf722953d6732233609def453e7d
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-03 22:10:47 +0100

    Update Header.jsx

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,3 +1,12 @@
-<header>
-  <h1>My Favorite Cities</h1>
-</header>
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+};
+
+// Make sure to export the Header component
+export default Header;
