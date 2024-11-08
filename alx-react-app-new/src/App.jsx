34ed0c556e8a834ebea6bb79843927bf5445c82e
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-08 11:23:55 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from './WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -17,6 +18,7 @@ function App() {
         <Header />
         <MainContent />
         <Footer />
+        <Counter />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
