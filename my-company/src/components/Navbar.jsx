068c1ec68b39930e701675fe8aa96c25b5479bb4
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-10 18:29:06 +0100

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,1 +1,26 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
+      <ul style={{ listStyleType: 'none', padding: 0, display: 'flex' }}>
+        <li style={{ margin: '0 10px' }}>
+          <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
+        </li>
+        <li style={{ margin: '0 10px' }}>
+          <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link>
+        </li>
+        <li style={{ margin: '0 10px' }}>
+          <Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link>
+        </li>
+        <li style={{ margin: '0 10px' }}>
+          <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
 
