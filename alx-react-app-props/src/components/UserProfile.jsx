6f5d88ccc54e08b97f4c502f128dda0f9dcb8a04
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-10 16:21:46 +0100

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,16 @@
-mport UserInfo from './UserInfo';
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
 
-function ProfilePage() {
-  return <UserInfo />;  {/* No need to pass userData as a prop anymore */}
+function UserDetails() {
+  // Access user data directly from the context
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default ProfilePage;
+export default UserDetails;
