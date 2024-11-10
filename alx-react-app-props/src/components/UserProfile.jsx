cb2a7714454a771b417f98b4d26612ac756ff2d4
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-10 16:17:51 +0100

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,30 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
+mport UserInfo from './UserInfo';
 
-const UserProfile = (props) => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  );
-};
+function ProfilePage() {
+  return <UserInfo />;  {/* No need to pass userData as a prop anymore */}
+}
 
-export default UserProfile;
-
-=======
-import React from 'react';
-
-const UserProfile = (props) => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  );
-};
-
-export default UserProfile;
->>>>>>> 2902277d7ba94087f323435bc5f09f331b4b35b4
+export default ProfilePage;
