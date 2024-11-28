9eff863782c7c233f0ad69d635b46129da21cd63
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-28 10:50:53 +0100

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,11 +1,16 @@
 // src/components/ProtectedRoute.jsx
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 
-function ProtectedRoute({ children, isAuthenticated }) {
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
   if (!isAuthenticated) {
+    // If the user is not authenticated, redirect to the login page (or home page)
     return <Navigate to="/" />;
   }
+
   return children;
 }
 
