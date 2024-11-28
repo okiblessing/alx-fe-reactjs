f1125175710835159621c7c8cc8184f383e2962a
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-28 10:32:39 +0100

    Update ProfileSettings.jsx

---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,8 @@
+// src/pages/ProfileSettings.jsx
+import React from 'react';
+
+function ProfileSettings() {
+  return <div>Settings for the user profile</div>;
+}
+
+export default ProfileSettings;
