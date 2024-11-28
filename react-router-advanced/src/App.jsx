7cb7f91f8108b9faa9e060a9309714d97ec8b026
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-11-28 10:34:50 +0100

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Post from './pages/Post';
 import Profile from './pages/Profile';
+import Post from './pages/Post';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
         <Route path="/post/:postId" element={<Post />} />
+        <Route 
+          path="/profile/*" 
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          } 
+        />
       </Routes>
     </Router>
   );
