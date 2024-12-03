528ab8c177113416e34822630173f8bbb48bf7bd
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-12-03 10:07:28 +0100

    Update tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./public/index.html",       // Ensure this is included if you have a public/index.html file
+    "./src/**/*.{js,jsx,ts,tsx}", // This tells Tailwind to look inside all JS, JSX, TS, and TSX files in the src directory
+  ],
   theme: {
     extend: {},
   },
