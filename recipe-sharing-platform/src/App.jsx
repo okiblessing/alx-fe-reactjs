95b5bafca2d597ec98bc50d22dbb0bae05ff0d90
okiblessing <164162837+okiblessing@users.noreply.github.com>
2024-12-05 09:03:26 +0100

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
@@ -12,5 +13,12 @@ function App() {
     </Router>
   );
 }
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-12 px-6">
+      <AddRecipeForm />
+    </div>
+  );
 
 export default App;
